d6a721b988b71f7821ed666b3eae30711593293d
Aashay Chaudhari <93089131+Aashay-chaudhari@users.noreply.github.com>
2024-05-02 19:06:41 -0400

    fix

--- readme.md
+++ readme.md
@@ -7,6 +7,8 @@ Updated 2nd May 2024
 1) Front end: Created a docker image for Angular frontend served on Nginx. The static content is available on port 80
 2) Back end: Created a docker image for Django to serve on 8000. The model is currently stored locally, will implement full automation and central model store solution (S3, GCS) later.
 
+#Restart docker-compose
+sudo docker-compose up --force-recreate --build -d
 
 
 #ARCHITECTURE OVERVIEW : 
--- stockapp_frontend/src/services/login.service.ts
+++ stockapp_frontend/src/services/login.service.ts
@@ -9,12 +9,12 @@ export class LoginService {
   constructor(private httpClient: HttpClient) { }
 
   addUser(param: any){
-    this.url = "https://backend:8000/addUser/"
+    this.url = "http://54.91.238.161:8000/addUser/"
     return this.httpClient.post(this.url, param);
   }
 
   checkUser(param: any){
-    this.url = "https://backend:8000/checkLogin/"
+    this.url = "http://54.91.238.161:8000/checkLogin/"
     return this.httpClient.post(this.url, param)
   }
 }
