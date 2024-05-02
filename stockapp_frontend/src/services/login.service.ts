1c6787d8aaf5485781a38e85a1571b01c42ed7bc
Aashay Chaudhari <93089131+Aashay-chaudhari@users.noreply.github.com>
2024-05-02 17:08:32 -0400

    fix

--- stockapp_frontend/src/services/login.service.ts
+++ stockapp_frontend/src/services/login.service.ts
@@ -9,12 +9,12 @@ export class LoginService {
   constructor(private httpClient: HttpClient) { }
 
   addUser(param: any){
-    this.url = "http://backend:8000/addUser/"
+    this.url = "http://3.81.234.31:8000/addUser/"
     return this.httpClient.post(this.url, param);
   }
 
   checkUser(param: any){
-    this.url = "http://backend:8000/checkLogin/"
+    this.url = "http://3.81.234.31:8000/checkLogin/"
     return this.httpClient.post(this.url, param)
   }
 }
